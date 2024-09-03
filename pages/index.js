9604745c28e2ad38c292e029b9289625e6db8104
Richard Lamb <lamby42@live.co.uk>
2024-09-03 13:34:31 +0100

    husky test commit3

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 // pages/index.js
-export default function Home() {
-    return <h1>Hello, Next.js!</h1>;
-  }
-  
+export    default function Home() {
+      return <h1>Hello, Next.js!</h1>;
+     }
   
