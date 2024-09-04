1fd6da0879965278ebb073ca75f06aafac20c092
Richard Lamb <lamby42@live.co.uk>
2024-09-04 11:30:36 +0100

    test lint and prettier

---
+++ src/components/Title.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Title from "./Title";
+import { render, screen } from "@testing-library/react";
+
+test("renders title", () => {
+    render(<Title text="Hello" />);
+    const titleElement = screen.getByText(/hello/i);
+    expect(titleElement).toBeInTheDocument();
+    });
